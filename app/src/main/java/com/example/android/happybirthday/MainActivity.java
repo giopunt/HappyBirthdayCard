ca43edd8d6313efd7c5358036eb706102c36253b
Giovanni Puntil <giovanni.puntil@asos.com>
2015-06-11 18:40:47 +0100

    HappyBirthday first commit

---
+++ app/src/main/java/com/example/android/happybirthday/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.android.happybirthday;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private SoundMeter mSensor;
+    private static final int POLL_INTERVAL = 300;
+
+
+    public int counter = 0;
+    public int mThreshold = 8;
+    private int mPollDelay = 1;
+
+    public AnimationDrawable frameAnimation;
+    private Handler mHandler = new Handler();
+
+    private Runnable mSleepTask = new Runnable() {
+        public void run() {
+            start();
+        }
+    };
+    private Runnable mPollTask = new Runnable() {
+        public void run() {
+            double amp = mSensor.getAmplitude();
+            if(amp > mThreshold){
+                counter++;
+            }
+            if(counter > 4){
+                Log.i("HappyBirthday", "ANIMATE CANDLE!!! ####   " + amp);
+            }
+            mHandler.postDelayed(mPollTask, POLL_INTERVAL);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView flameView = (ImageView) findViewById(R.id.flame);
+        flameView.setBackgroundResource(R.drawable.animated_flame);
+        frameAnimation = (AnimationDrawable) flameView.getBackground();
+
+        mSensor = new SoundMeter();
+        start();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            frameAnimation.start();
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        stop();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        frameAnimation.start();
+    }
+
+    private void start() {
+        counter = 0;
+        mSensor.start();
+        mHandler.postDelayed(mPollTask, POLL_INTERVAL);
+    }
+
+    private void stop() {
+        mHandler.removeCallbacks(mSleepTask);
+        mHandler.removeCallbacks(mPollTask);
+        mSensor.stop();
+    }
+
+    private void sleep() {
+        mSensor.stop();
+        mHandler.postDelayed(mSleepTask, 1000*mPollDelay);
+    }
+
+}
---
+++ app/src/main/java/com/example/android/happybirthday/SoundMeter.java
@@ -0,0 +1,52 @@
+package com.example.android.happybirthday;
+
+import android.media.MediaRecorder;
+import java.io.IOException;
+
+/**
+ * Created by Giovanni.Puntil on 11/06/2015.
+ */
+public class SoundMeter {
+    static final private double EMA_FILTER = 0.6;
+
+    private MediaRecorder mRecorder = null;
+    private double mEMA = 0.0;
+
+    public void start() {
+        if (mRecorder == null) {
+            mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mRecorder.setOutputFile("/dev/null");
+            try {
+                mRecorder.prepare();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mRecorder.start();
+        }
+    }
+
+    public void stop() {
+        if (mRecorder != null) {
+            mRecorder.stop();
+            mRecorder.release();
+            mRecorder = null;
+        }
+    }
+
+    public double getAmplitude() {
+        if (mRecorder != null)
+            return  (mRecorder.getMaxAmplitude()/2700.0);
+        else
+            return 0;
+
+    }
+
+    public double getAmplitudeEMA() {
+        double amp = getAmplitude();
+        mEMA = EMA_FILTER * amp + (1.0 - EMA_FILTER) * mEMA;
+        return mEMA;
+    }
+}
